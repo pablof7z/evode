9016929ee2fa09d76883246d2feff54e4edb484d
Pablo Fernandez <heelhook@littleq.net>
2011-11-17 01:52:06 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evode::Application.config.secret_token = '1acab6b9f6493cf384a17bca770b2c0c326b0d980fc1bd3e25bc350b73f0c815a0283331baa7ce2f7a868d3fea55e925a58f5624ad09c430ec7cef2c9e134e30'
